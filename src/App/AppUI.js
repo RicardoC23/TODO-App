3688b56635a035531b3f28c5159c405d4366466c
RicardoC23 <ricardomamani403@gmail.com>
2023-03-09 21:37:08 -0500

    TODO completed

---
+++ src/App/AppUI.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { TodoContext } from '../TodoContext';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodoForm } from '../TodoForm';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
+
+
+
+function AppUI () {
+  const { 
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext)
+
+    return (
+
+      <div className="Container">
+
+        <TodoCounter/>
+        <TodoSearch/>
+
+        <TodoList>
+          {error && <p>Desesperate, hubo un error... </p>}
+          {loading && <p>Estamos cargando, no desesperes... </p>}
+          {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO!</p>}
+          {searchedTodos.map((todo,index) => (
+        <TodoItem
+          key = {index}
+          text = {todo.text}
+          completed = {todo.completed}
+          onComplete = {() => completeTodo(todo.text)}
+          onDelete = {() => deleteTodo(todo.text)}
+        />
+          ))}
+        </TodoList>
+        {!!openModal && (
+            <Modal>
+              <TodoForm/>
+            </Modal>
+        )}
+        <CreateTodoButton
+          setOpenModal= {setOpenModal}
+        
+        />
+      
+      </div>
+    );
+}
+
+export { AppUI }
